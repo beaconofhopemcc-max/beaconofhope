5ca895511a421a32fd6f2ff6064c81f425fb16de
courageoususahealth-rgb <courageoususahealth@gmail.com>
2025-12-21 07:13:16 +0100

    hi
    hi

---
+++ src/app/sitemap.js
@@ -0,0 +1,81 @@
+export default async function sitemap() {
+  const baseUrl = "https://www.beaconofhopepsychiatrymcc.com";
+
+  return [
+    {
+      url: `${baseUrl}/`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/adhd`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/anxiety`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/bipolar-disorder`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/ocd`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/book-consultation`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/contact`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/depression`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/ptsd`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/insurance`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/ocd`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/teams`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/patient-intake`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/postpartum-depression`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/privacy-policy`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/ptsd`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/services`,
+      lastModified: new Date()},
+    {
+      url: `${baseUrl}/substance-abuse`,
+      lastModified: new Date(),
+    },
+  ];
+}
